7843be9483fa31bab193c01bacbbe6f687cbb467
Rodrigo Ditrich <rodrigo.jonas.dittrich@gmail.com>
2021-08-31 14:14:15 -0300

    #2 - Modules

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
-//import 'package:module_home/app/home_module.dart';
-//import 'package:module_login/app/login_module.dart';
+import 'package:module_home/app/home_module.dart';
+import 'package:module_login/app/login_module.dart';
 
 class AppModule extends Module {
   @override
@@ -9,8 +9,8 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    //ModuleRoute('/login', module: LoginModule()),
-    //ModuleRoute('/home', module: HomeModule()),
-    //ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute('/login', module: LoginModule()),
+    ModuleRoute('/home', module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: HomeModule()),
   ];
 }
